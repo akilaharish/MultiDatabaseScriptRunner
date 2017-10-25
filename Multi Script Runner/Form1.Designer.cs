d628137c7a9f711783ea132a47a880c85ea3f245
akila <akila@AURBLRLT-185>
2017-10-25 17:10:44 +0530

    Add project files.

---
+++ Multi Script Runner/Form1.Designer.cs
@@ -0,0 +1,245 @@
+﻿namespace Multi_Script_Runner
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lstDbs = new System.Windows.Forms.ListBox();
+            this.btnConnect = new System.Windows.Forms.Button();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.lblPassword = new System.Windows.Forms.Label();
+            this.txtUID = new System.Windows.Forms.TextBox();
+            this.lblUID = new System.Windows.Forms.Label();
+            this.txtServer = new System.Windows.Forms.TextBox();
+            this.lblServer = new System.Windows.Forms.Label();
+            this.lblQuery = new System.Windows.Forms.Label();
+            this.txtQuery = new System.Windows.Forms.TextBox();
+            this.btnExecute = new System.Windows.Forms.Button();
+            this.panel2 = new System.Windows.Forms.FlowLayoutPanel();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.lstDbs);
+            this.panel1.Controls.Add(this.btnConnect);
+            this.panel1.Controls.Add(this.txtPassword);
+            this.panel1.Controls.Add(this.lblPassword);
+            this.panel1.Controls.Add(this.txtUID);
+            this.panel1.Controls.Add(this.lblUID);
+            this.panel1.Controls.Add(this.txtServer);
+            this.panel1.Controls.Add(this.lblServer);
+            this.panel1.Location = new System.Drawing.Point(591, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(258, 597);
+            this.panel1.TabIndex = 1;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.ForeColor = System.Drawing.Color.Red;
+            this.label3.Location = new System.Drawing.Point(42, 74);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(11, 13);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "*";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.ForeColor = System.Drawing.Color.Red;
+            this.label2.Location = new System.Drawing.Point(42, 46);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(11, 13);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "*";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.ForeColor = System.Drawing.Color.Red;
+            this.label1.Location = new System.Drawing.Point(42, 16);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(11, 13);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "*";
+            // 
+            // lstDbs
+            // 
+            this.lstDbs.FormattingEnabled = true;
+            this.lstDbs.Location = new System.Drawing.Point(7, 136);
+            this.lstDbs.Name = "lstDbs";
+            this.lstDbs.SelectionMode = System.Windows.Forms.SelectionMode.MultiExtended;
+            this.lstDbs.Size = new System.Drawing.Size(248, 459);
+            this.lstDbs.TabIndex = 7;
+            // 
+            // btnConnect
+            // 
+            this.btnConnect.Location = new System.Drawing.Point(30, 107);
+            this.btnConnect.Name = "btnConnect";
+            this.btnConnect.Size = new System.Drawing.Size(195, 23);
+            this.btnConnect.TabIndex = 6;
+            this.btnConnect.Text = "Connect";
+            this.btnConnect.UseVisualStyleBackColor = true;
+            this.btnConnect.Click += new System.EventHandler(this.btnConnect_Click);
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.Location = new System.Drawing.Point(55, 71);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.PasswordChar = '*';
+            this.txtPassword.Size = new System.Drawing.Size(200, 20);
+            this.txtPassword.TabIndex = 5;
+            // 
+            // lblPassword
+            // 
+            this.lblPassword.AutoSize = true;
+            this.lblPassword.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPassword.Location = new System.Drawing.Point(7, 74);
+            this.lblPassword.Name = "lblPassword";
+            this.lblPassword.Size = new System.Drawing.Size(36, 13);
+            this.lblPassword.TabIndex = 4;
+            this.lblPassword.Text = "PWD";
+            // 
+            // txtUID
+            // 
+            this.txtUID.Location = new System.Drawing.Point(55, 43);
+            this.txtUID.Name = "txtUID";
+            this.txtUID.Size = new System.Drawing.Size(200, 20);
+            this.txtUID.TabIndex = 3;
+            // 
+            // lblUID
+            // 
+            this.lblUID.AutoSize = true;
+            this.lblUID.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblUID.Location = new System.Drawing.Point(7, 46);
+            this.lblUID.Name = "lblUID";
+            this.lblUID.Size = new System.Drawing.Size(29, 13);
+            this.lblUID.TabIndex = 2;
+            this.lblUID.Text = "UID";
+            // 
+            // txtServer
+            // 
+            this.txtServer.Location = new System.Drawing.Point(55, 13);
+            this.txtServer.Name = "txtServer";
+            this.txtServer.Size = new System.Drawing.Size(200, 20);
+            this.txtServer.TabIndex = 1;
+            // 
+            // lblServer
+            // 
+            this.lblServer.AutoSize = true;
+            this.lblServer.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblServer.Location = new System.Drawing.Point(4, 13);
+            this.lblServer.Name = "lblServer";
+            this.lblServer.Size = new System.Drawing.Size(44, 13);
+            this.lblServer.TabIndex = 0;
+            this.lblServer.Text = "Server";
+            // 
+            // lblQuery
+            // 
+            this.lblQuery.AutoSize = true;
+            this.lblQuery.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblQuery.Location = new System.Drawing.Point(4, 12);
+            this.lblQuery.Name = "lblQuery";
+            this.lblQuery.Size = new System.Drawing.Size(40, 13);
+            this.lblQuery.TabIndex = 2;
+            this.lblQuery.Text = "Query";
+            // 
+            // txtQuery
+            // 
+            this.txtQuery.Location = new System.Drawing.Point(7, 29);
+            this.txtQuery.Multiline = true;
+            this.txtQuery.Name = "txtQuery";
+            this.txtQuery.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.txtQuery.Size = new System.Drawing.Size(578, 188);
+            this.txtQuery.TabIndex = 3;
+            // 
+            // btnExecute
+            // 
+            this.btnExecute.Location = new System.Drawing.Point(135, 224);
+            this.btnExecute.Name = "btnExecute";
+            this.btnExecute.Size = new System.Drawing.Size(337, 23);
+            this.btnExecute.TabIndex = 4;
+            this.btnExecute.Text = "Execute";
+            this.btnExecute.UseVisualStyleBackColor = true;
+            this.btnExecute.Click += new System.EventHandler(this.btnExecute_Click);
+            // 
+            // panel2
+            // 
+            this.panel2.AutoScroll = true;
+            this.panel2.Location = new System.Drawing.Point(12, 253);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(573, 356);
+            this.panel2.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(861, 621);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.btnExecute);
+            this.Controls.Add(this.txtQuery);
+            this.Controls.Add(this.lblQuery);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Multi Script Runner";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label lblServer;
+        private System.Windows.Forms.TextBox txtServer;
+        private System.Windows.Forms.Label lblUID;
+        private System.Windows.Forms.TextBox txtUID;
+        private System.Windows.Forms.TextBox txtPassword;
+        private System.Windows.Forms.Label lblPassword;
+        private System.Windows.Forms.Button btnConnect;
+        private System.Windows.Forms.ListBox lstDbs;
+        private System.Windows.Forms.Label lblQuery;
+        private System.Windows.Forms.TextBox txtQuery;
+        private System.Windows.Forms.Button btnExecute;
+        private System.Windows.Forms.FlowLayoutPanel panel2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Multi Script Runner/Form1.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace Multi_Script_Runner
+{
+    public partial class Form1 : Form
+    {
+        bool IsConnected = false;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnConnect_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (string.IsNullOrEmpty(txtServer.Text) || string.IsNullOrEmpty(txtUID.Text) || string.IsNullOrEmpty(txtPassword.Text))
+                {
+                    MessageBox.Show("Please enter the required details.");
+                    IsConnected = false;
+                }
+                else
+                {
+                    lstDbs.Items.Clear();
+                    SqlConnection SqlCon = new SqlConnection("server=" + txtServer.Text + ";uid=" + txtUID.Text + ";pwd=" + txtPassword.Text);
+                    SqlCon.Open();
+                    SqlCommand SqlCom = new SqlCommand();
+                    SqlCom.Connection = SqlCon;
+                    SqlCom.CommandType = CommandType.StoredProcedure;
+                    SqlCom.CommandText = "sp_databases";
+
+                    System.Data.SqlClient.SqlDataReader SqlDR;
+                    SqlDR = SqlCom.ExecuteReader();
+
+                    while (SqlDR.Read())
+                    {
+                        lstDbs.Items.Add(SqlDR.GetString(0));
+                    }
+
+                    SqlDR.Close();
+                    SqlCon.Close();
+
+                    IsConnected = true;
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+                IsConnected = false;
+            }
+        }
+
+        private void btnExecute_Click(object sender, EventArgs e)
+        {
+            if (!IsConnected)
+            {
+                MessageBox.Show("Please connect to the server before executing scripts.");
+            }
+            else
+            {
+                panel2.Controls.Clear();
+                Point Point = new Point(0, 0);
+
+                foreach (object obj in lstDbs.SelectedItems)
+                {
+                    SqlConnection SqlCon = new SqlConnection("server=" + txtServer.Text + ";uid=" + txtUID.Text + ";pwd=" + txtPassword.Text + ";Database=" + obj.ToString());
+                    SqlCon.Open();
+
+                    SqlCommand SqlCom = new SqlCommand();
+                    SqlCom.Connection = SqlCon;
+                    SqlCom.CommandType = CommandType.Text;
+                    SqlCom.CommandText = txtQuery.Text;
+
+                    DataSet ds = new DataSet();
+                    SqlDataAdapter SqlDA = new SqlDataAdapter(SqlCom);
+                    SqlDA.Fill(ds);
+
+                    Label lbl = new Label();
+                    panel2.Controls.Add(lbl);
+                    lbl.Text = "Query executed on DB " + obj.ToString();
+                    lbl.Location = new Point(0, panel2.AutoScrollPosition.Y + Point.Y);
+                    lbl.Font = new Font(lbl.Font, FontStyle.Bold);
+                    lbl.Width = 500;
+                    Point.Y += 30;
+
+                    if (ds.Tables.Count > 0)
+                    {
+                        DataGridView dg1 = new DataGridView();
+                        panel2.Controls.Add(dg1);
+
+                        dg1.DataSource = ds.Tables[0];
+                        dg1.AutoGenerateColumns = true;
+                        dg1.Name = obj.ToString() + "_DB";
+                        dg1.Height = 200;
+                        dg1.Width = 550;
+                        dg1.Location = new Point(0, panel2.AutoScrollPosition.Y + Point.Y);
+                        Point.Y += 200;
+                    }
+
+                    SqlCon.Close();
+                }
+            }
+        }
+    }
+}
